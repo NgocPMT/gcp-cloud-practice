1db56043f0e985ec74a31bd7545f175cff73eb9c
Nathan <np1552005@gmail.com>
2026-03-05 13:48:45 +0700

    chore: import dotenv for local development

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { existsSync, readFileSync } from 'fs';
+import 'dotenv/config';
 
 let dbUrl = process.env.DATABASE_URL;
 
